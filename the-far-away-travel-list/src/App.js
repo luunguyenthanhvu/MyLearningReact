55c1b1d7b01cfa89faf89aedd1ddd47cb5853d01
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-03-14 01:39:41 +0700

    push some new update code

--- the-far-away-travel-list/src/App.js
+++ the-far-away-travel-list/src/App.js
@@ -1,10 +1,15 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
-      <Logo></Logo>
-      <Form></Form>
-      <PackingList></PackingList>
-      <Stats></Stats>
+      <Logo/>
+      <Form/>
+      <PackingList/>
+      <Stats/>
     </div>
   );
 }
@@ -25,13 +30,31 @@ function Form() {
 
 function PackingList() {
   return (
-    <div className="list">List</div>
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item = {item}/>
+        ))}
+      </ul>
+    </div>
   );
 }
 
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>
+        X
+      </button>
+    </li>
+  );
+}
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your lists, and you already packed X </em>
     </footer>
   );
